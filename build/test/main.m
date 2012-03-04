79d941f5982517a7d718c90a0d1512cb0107baba
Alex Gordon <anything@fileability.net>
2012-03-04 15:53:37 +0000

    Test hooks

--- build/test/main.m
+++ build/test/main.m
@@ -2,19 +2,6 @@
 #include "+support.h"
 
 
-#pragma mark isKind (object)
-static void test_isKind_object() {
-    ass( isKind(foo, @"NSString") );
-    ass( isKind(foo, [NSString class]) );
-    ass( isKind(bar, @"NSNumber") );
-    ass( isKind(bar, [NSNumber class]) );
-    ass( isKind(baz, @"NSArray") );
-    ass( isKind(baz, [NSArray class]) );
-    ass( isKind([NSDate date], @"NSDate") );
-    ass( isKind([NSDate date], [NSDate class]) );
-}
-
-
 #pragma mark map (iter)
 static void test_map_iter() {
     ass  ( !map(nil, muttoupper) );
@@ -23,17 +10,7 @@ static void test_map_iter() {
 }
 
 
-#pragma mark responds (object)
-static void test_responds_object() {
-    ass( responds(foo, @selector(characterAtIndex:)) );
-    ass( responds(bar, @selector(isEqualToNumber:)) );
-    ass( responds(baz, @selector(objectAtIndex:)) );
-}
-
-
 #pragma mark main
 int main(void) {
-    test_isKind_object();
     test_map_iter();
-    test_responds_object();
 }
