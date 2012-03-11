56813454548326c66eff344972543a839f55e42b
Alex Gordon <anything@fileability.net>
2012-03-11 02:20:43 +0000

    Wishlists

--- development/wishlist.py
+++ development/wishlist.py
@@ -10,16 +10,16 @@ def clean(ls):
 wl = open("wishlist2.txt", 'r').read()
 allfuncs = set(wl.splitlines())
 
-stable = clean(os.listdir('../source/stable'))
-unstable = clean(os.listdir('../source/unstable'))
+stable = set(x for x in clean(os.listdir('../source/stable')) if x != 'DS_Sto')
+unstable = set(x for x in clean(os.listdir('../source/unstable')) if x != 'DS_Sto')
 desired = set(x for x in (allfuncs - stable - unstable) if x != '')
 
 fl = open("wishlist_computed.txt", 'w')
 fl.write("DESIRED functions\n  ")
-fl.write('\n  '.join(desired) + '\n\n')
+fl.write('\n  '.join(sorted(desired)) + '\n\n')
 fl.write("UNSTABLE functions\n  ")
-fl.write('\n  '.join(unstable) + '\n\n')
+fl.write('\n  '.join(sorted(unstable)) + '\n\n')
 fl.write("STABLE functions\n  ")
-fl.write('\n  '.join(stable) + '\n\n')
+fl.write('\n  '.join(sorted(stable)) + '\n\n')
 
 desired
